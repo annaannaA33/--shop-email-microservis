4dfd0c7c16403ebbfa2b7122468d1b97167ca357
annaannaA33 <annaansmva@gmail.com>
2024-10-01 18:10:22 +0300

    fix

--- email-controller.js
+++ email-controller.js
@@ -1,6 +1,14 @@
 const nodemailer = require("nodemailer");
 const dotenv = require("dotenv");
 dotenv.config();
+const { logger, setLogLevel } = require("./logger/logger");
+setLogLevel("DEBUG");
+
+logger.debug(`Loading SMTP settings...`);
+logger.debug(`SMTP_HOST: ${process.env.SMTP_HOST}`);
+logger.debug(`SMTP_PORT: ${process.env.SMTP_PORT}`);
+logger.debug(`SENDER_EMAIL: ${process.env.SENDER_EMAIL}`);
+logger.debug(`SENDER_EMAIL_PASS: ${process.env.SENDER_EMAIL_PASS}`);
 
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
@@ -12,7 +20,14 @@ const transporter = nodemailer.createTransport({
     },
 });
 
+logger.debug(`SMTP transporter initialized`);
+
 const sendEmail = async (to, subject, text) => {
+    logger.debug(`Starting email sending process...`);
+    logger.debug(`From: "${process.env.SENDER_EMAIL}"`);
+    logger.debug(`To: ${to}`);
+    logger.debug(`Subject: ${subject}`);
+    logger.debug(`Text: ${text.length > 50 ? "..." : text}`);
     try {
         const info = await transporter.sendMail({
             from: '"SomeName" <SENDER_EMAIL>',
@@ -21,9 +36,15 @@ const sendEmail = async (to, subject, text) => {
             text,
         });
         console.log("Message sent: %s", info.messageId);
+        logger.debug(`Message sent: %s`, info.messageId);
+        logger.debug(`Response status: ${info.response}`);
+        logger.debug(`Response code: ${info.code}`);
+        logger.debug(`Response message: ${info.message}`);
         return info;
     } catch (error) {
         console.error("Error sending email:", error);
+        logger.error("Error sending email:", error);
+        logger.error("Error stack trace:", error.stack);
         throw error;
     }
 };
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const { sendEmail } = require("./email-controller");
 const { logger, setLogLevel } = require("./logger/logger");
-//const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-const nodemailer = require("nodemailer");
+const cors = require("cors");
 
+setLogLevel("DEBUG");
 const app = express();
 app.use(express.json());
-//app.use(bodyParser.json());
-//dotenv.config();
+app.use(cors());
+dotenv.config();
 
 app.post("/sendemail", async (req, res) => {
     logger.info(
@@ -16,15 +16,29 @@ app.post("/sendemail", async (req, res) => {
             req.body
         )}`
     );
+    logger.debug(`Request body: ${JSON.stringify(req.body)}`);
     const { to, subject, text } = req.body;
-
+    logger.debug(
+        `Extracted email details: to=${to}, subject=${subject}, text=${
+            text.length > 50 ? "..." : text
+        }`
+    );
     try {
-        await sendEmail(to, subject, text);
-        logger.debug();
-        res.status(200).send("Email sent successfully!");
+        const result = await sendEmail(to, subject, text);
+        logger.debug(`Attempting to send email to: ${to}, Subject: ${subject}`);
+
+        if (result) {
+            logger.info("Email sent successfully!");
+            res.status(200).send("Email sent successfully!");
+        } else {
+            logger.error("Failed to send email");
+            res.status(500).send("Error sending email.");
+        }
     } catch (error) {
+        logger.error(`Error occurred while sending email: ${error.message}`);
+        logger.error(`Error stack trace:`, error.stack);
+
         res.status(500).send("Error sending email.");
-        logger.debug();
     }
 });
 
@@ -32,7 +46,7 @@ app.get("/health", (req, res) => {
     res.send({ status: "Email service is up and running" });
 });
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Email service running on port ${PORT}`);
 });
