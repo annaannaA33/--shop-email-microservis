3bddff55d16195b9e417ea28ec63a6c2c2874d0b
annaannaA33 <annaansmva@gmail.com>
2024-10-01 15:53:56 +0300

    test

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get("/health", (req, res) => {
     res.send({ status: "Email service is up and running" });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
     console.log(`Email service running on port ${PORT}`);
 });
